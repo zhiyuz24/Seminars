7c609250540142cd98cae38efe19363ec8bc1d1e
Jozsef Zsiros <44138362+ardanwan@users.noreply.github.com>
2020-09-23 18:07:18 +0200

    Update assignment_2c.py

--- assignment_2c.py
+++ assignment_2c.py
@@ -1,4 +1,9 @@
-
+"""
+Write a documentation for the simple function below. Your partner will have to
+implement the function, without knowing the code. Send your partner the
+documentation and see if he can work with it.
+No cheating! Don't show or tell hem the code directly
+"""
 def function_2c(w, x, y, z):
 
     multiplication = x * y
